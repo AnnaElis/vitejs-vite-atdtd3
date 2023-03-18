46f4cfb135aab485b1eb757fcdbdd87dd941dc83
AnnaElis <44591488+AnnaElis@users.noreply.github.com>
2023-03-19 01:13:52 +0300

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [likes, setLikes] = useState (initialState, 5)
-  function increment () {
-    setLikes (value, likes + 1)
+  const [likes, setLikes] = useState(initialState, 5);
+  function increment() {
+    setLikes(value, likes + 1);
   }
-  function decrement () {
-    setLikes (value, likes + 1)
+  function decrement() {
+    setLikes(value, likes + 1);
   }
   return (
     <div className="App">
       <h1>{likes}</h1>
-     <button onClick={increment}>Increment</button>
-     <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
